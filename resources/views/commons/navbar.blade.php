8b8b4ff18239df14d81c621bcd76deee3c9e6ef5
平松暢顕 <hiramatsunobuaki@hiramatsutoorukennoMacBook-Pro.local>
2021-03-05 17:33:28 +0900

    top-page

---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,16 @@
+<header>
+    <nav class="navbar navbar-expand-md">
+        <a href="" class="navbar-brand font-weight-bold">Football Expansion</a>
+        <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#nav-bar" aria-controls="nav-bar" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="nav-bar">
+            <ul class="navbar-nav">
+                {{-- ユーザ登録のリンク--}}
+                <li class="nav-item"><a href="" class="nav-link font-weight-bold" data-toggle="collapse" data-target=".navbar-collapse.show">ユーザー登録</a></li>
+                {{-- ログインのリンク　--}}
+                <li class="nav-item"><a href="" class="nav-link font-weight-bold" data-toggle="collapse" data-target=".navbar-collapse.show">ログイン</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
