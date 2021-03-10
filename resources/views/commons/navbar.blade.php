f917c2dab3cb8d3966da81049945d2128950c81a
平松暢顕 <hiramatsunobuaki@hiramatsutoorukennoMacBook-Pro.local>
2021-03-10 17:46:22 +0900

    loginlogout

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -1,15 +1,22 @@
 <header>
     <nav class="navbar navbar-expand-md">
-        <a href="" class="navbar-brand font-weight-bold">Football Expansion</a>
+        <a href="/" class="navbar-brand font-weight-bold">Football Expansion</a>
         <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#nav-bar" aria-controls="nav-bar" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse justify-content-end" id="nav-bar">
             <ul class="navbar-nav">
-                {{-- ユーザ登録のリンク--}}
-                <li>{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link font-weight-bold']) !!}</li>
-                {{-- ログインのリンク　--}}
-                <li class="nav-item"><a href="" class="nav-link font-weight-bold" data-toggle="collapse" data-target=".navbar-collapse.show">Login</a></li>
+                @if (Auth::check())
+                {{-- ログイン済みの場合の表示　--}}
+                    {{--　ログアウトのリンク　--}}
+                    <li class="nav-item">{!! link_to_route('logout.get', 'Logout', [], ['class' => 'nav-link font-weight-bold']) !!}</li>
+                @else
+                {{--　未ログインの場合の表示　--}}
+                    {{-- ユーザ登録のリンク--}}
+                    <li class="nav-item">{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link font-weight-bold']) !!}</li>
+                    {{-- ログインのリンク　--}}
+                    <li class="nav-item">{!! link_to_route('login', 'Login', [], ['class' => 'nav-link font-weight-bold']) !!}</li>
+                @endif    
             </ul>
         </div>
     </nav>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="text-center mt-4">
-        <h1>Welcome to FOOTBALL EXPANSION</h1>
-        <p>このサイトではプレミアリーグ、ラ・リーガ、セリエAのリアルタイムの順位表、試合日程をまとめてチェックすることができます。</p>
-        <p>また、自分のお気に入りのチームを登録することで、試合日程、試合結果をお気に入りチームだけに絞って閲覧できます</p>
-        <p>これを利用して、より優雅な海外サッカー視聴ライフを実現しよう！！</p>
+    @if (Auth::check())
+    {{--ログインできている場合--}}
+        {{ Auth::user()->name }}
+    @else
+    {{--ログインできていない場合--}}
+        <div class="text-center mt-4">
+            <h1>Welcome to FOOTBALL EXPANSION</h1>
+            <p>このサイトではプレミアリーグ、ラ・リーガ、セリエAのリアルタイムの順位表、試合日程をまとめてチェックすることができます。</p>
+            <p>また、自分のお気に入りのチームを登録することで、試合日程、試合結果をお気に入りチームだけに絞って閲覧できます</p>
+            <p>これを利用して、より優雅な海外サッカー視聴ライフを実現しよう！！</p>
 
-        {{-- ユーザ登録ページへのリンク --}}
-        {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-md btn-primary']) !!}
-    </div>
+            {{-- ユーザ登録ページへのリンク --}}
+            {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-md btn-primary']) !!}
+        </div>
+    @endif
 @endsection
